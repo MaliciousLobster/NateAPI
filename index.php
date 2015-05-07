1e213cca175d1c18774865ed836b644022bffbb5
Nate M <Mr.awsmness@gmail.com>
2015-05-07 11:21:43 -0700

    API: milestone 4

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //make constants using define
-define('client_ID', 'a961d8bb8147413396a2257206bbdb1e');
-define('client_Secret', 'ebacc95ad11f4d588492f5a6c38953b4');
+define('clientID', 'a961d8bb8147413396a2257206bbdb1e');
+define('clientSecret', 'ebacc95ad11f4d588492f5a6c38953b4');
 define('redirectURI', 'http://localhost/NateAPI/index.php');
 define('ImageDirectory', 'pics/');
 
@@ -22,6 +22,6 @@ define('ImageDirectory', 'pics/');
 <body>
 	<!--creating a login to go and give approval to access instagram profile
 	after approval, now the information are usable.  -->
-	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN</a>
+	<a href="https:api.instagram.com/oauth.authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code/">LOGIN</a>
 </body>
 </html>
